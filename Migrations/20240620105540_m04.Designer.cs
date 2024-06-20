0590f6c2c888a0795ea88c4d211a5f15ecd51d3e
Guillermo Castro de la Puente <valkianceo@outlook.com>
2024-06-20 12:57:25 +0200

    dba 1.1.0

--- Migrations/20240620105540_m04.Designer.cs
+++ Migrations/20240620105540_m04.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using dba.Data;
 
@@ -11,9 +12,11 @@ using dba.Data;
 namespace dba.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240620105540_m04")]
+    partial class m04
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
